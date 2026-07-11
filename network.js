79b42eec879e14551c6356918e4746c5dbb30062
habibu070A <habibusulaiman6778@gmail.com>
2026-07-12 00:31:07 +0100

    Fix logo paths for cryptocurrency entries
    Updated logo paths for cryptocurrency entries to use relative paths without '../photos/'.

--- network.js
+++ network.js
@@ -2,15 +2,15 @@
 
 export let coins = [
   
-  {id: "tether", logo: "../photos/usdt.png", name: "Tether USDT", symbol: "USDT", network: "BSC", contract: null, balance: 0, price: 0, change: 0},
+  {id: "tether", logo: "./usdt.png", name: "Tether USDT", symbol: "USDT", network: "BSC", contract: null, balance: 0, price: 0, change: 0},
   
-  {id: "bitcoin", logo: "../photos/btc.png", name: "Bitcoin BTC", symbol: "BTC", network: "BSC", contract: null, balance: 0, price: 0, change: 0},
+  {id: "bitcoin", logo: "./btc.png", name: "Bitcoin BTC", symbol: "BTC", network: "BSC", contract: null, balance: 0, price: 0, change: 0},
   
-  {id: "ton", logo: "../photos/ton.png", name: "Ton Network", symbol: "Ton", network: "ton", cobtract: null, balance: 0, price: 0, change: 0},
+  {id: "ton", logo: "./ton.png", name: "Ton Network", symbol: "Ton", network: "ton", cobtract: null, balance: 0, price: 0, change: 0},
   
-  {id: "solana", logo: "../photos/sol.png", name: "solana", symbol: "Sol", network: "solana", contract: null, balance: 0, price: 0, change: 0},
+  {id: "solana", logo: "./sol.png", name: "solana", symbol: "Sol", network: "solana", contract: null, balance: 0, price: 0, change: 0},
   
   {id: "ethereum", logo: "./eth.png", name: "Ethereum", symbol: "ETH", contract: null, balance: 0, price: 0, change: 0},
   
-  {id: "binancecoin", logo: "../photos/bnb.png", name: "Bnb Smart Chain", symbol: "BNB", contract: null, balance: 0, price: 0, change: 0}
+  {id: "binancecoin", logo: "./bnb.png", name: "Bnb Smart Chain", symbol: "BNB", contract: null, balance: 0, price: 0, change: 0}
 ]
